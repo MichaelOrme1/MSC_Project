3e9429309264b0266adddf6f52bb42b93965b9b5
MichaelOrme1 <99344686+MichaelOrme1@users.noreply.github.com>
2022-08-08 14:00:02 +0100

    Add files via upload

--- Changed Retro Files/retro_env.py
+++ Changed Retro Files/retro_env.py
@@ -10,7 +10,29 @@ import retro.data
 from gym.utils import seeding
 from itertools import product
 #from rominfo import *
+"""
+The MIT License
 
+Copyright (c) 2017-2018 OpenAI (http://openai.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 gym_version = tuple(int(x) for x in gym.__version__.split('.'))
 
 __all__ = ['RetroEnv']
@@ -150,6 +172,7 @@ class RetroEnv(gym.Env):
         elif self._obs_type == retro.Observations.IMAGE:
             self.img = self.get_screen()
             return self.img
+        #New Code
         elif self._obs_type == retro.Observations.RAM2:
             ram = self.getRam()
             state, x, y = self.getInputs(ram)
@@ -318,7 +341,7 @@ class RetroEnv(gym.Env):
 
 
 
-
+    #New Code
     def getInputs(self,ram, radius=6):
       '''
       getInputs(ram): returns an nd.array of enemies, obstacles within a radius around the agent
